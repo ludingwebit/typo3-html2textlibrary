2681c487eaa6ed34dd4490ac88dead72304737ba
Dan Untenzu <untenzu@webit.de>
2018-04-05 17:04:11 +0200

    [WIP][FEATURE] Add draft
    Add draft as of 2016-01-12.

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "formhandler_clearlog_task".
+#
+# Auto generated 18-12-2013 15:30
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'HTML to TXT Library',
+	'description' => 'Creates a plain text version of a webpage using the html2text library',
+	'category' => 'misc',
+	'author' => 'Dan Untenzu',
+	'author_email' => 'untenzu@webit.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.1.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-7.6.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
+
+?>
